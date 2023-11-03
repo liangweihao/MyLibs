fd787d7dd93dafcf9fbd056fd1e57932ec8144a5
liangweihao <>
2023-11-03 19:28:45 +0800

    update project

---
+++ demo/build/generated/source/buildConfig/debug/com/nothing/demo/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.nothing.demo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.nothing.demo";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 11;
+  public static final String VERSION_NAME = "1.1.1";
+}
---
+++ demo/src/main/java/com/nothing/demo/AndroidServerActivity.kt
@@ -0,0 +1,14 @@
+package com.nothing.demo
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+
+class AndroidServerActivity: FragmentActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+}
